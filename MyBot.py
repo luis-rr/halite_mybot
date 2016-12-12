633d32f5fe4095a42fa3ab0e76c820f6e80e1aee
Luis Riquelme <jluis.riquelme@gmail.com>
2016-12-12 21:51:37 +0100

    reorganise bot code in single file

--- MyBot.py
+++ MyBot.py
@@ -1,34 +1,3 @@
-import hlt
-from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
-import random
+from bots import BlobBot as MyBot
 
-
-class Hive(object):
-    def __init__(self):
-
-        self.my_id, self.game_map = hlt.get_init()
-        hlt.send_init("PythonBot")
-
-    def assign_move(self, square):
-        for direction, neighbor in enumerate(self.game_map.neighbors(square)):
-            if neighbor.owner != self.my_id and neighbor.strength < square.strength:
-                return Move(square, direction)
-
-        if all(neighbor.owner == self.my_id for neighbor in self.game_map.neighbors(square)):
-            return Move(square, random.choice((NORTH, WEST, SOUTH, EAST)))
-
-        return Move(square, STILL)
-
-
-def run():
-
-    hive = Hive()
-
-    while True:
-        hive.game_map.get_frame()
-
-        moves = [hive.assign_move(square) for square in hive.game_map if square.owner == hive.my_id]
-        hlt.send_frame(moves)
-
-
-run()
+MyBot().run()
---
+++ bots.py
@@ -0,0 +1,43 @@
+import hlt
+from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
+import random
+
+
+class Bot(object):
+    def __init__(self, name):
+        self.my_id, self.game_map = hlt.get_init()
+        hlt.send_init(name)
+
+    def assign_move(self, square):
+        return Move(square, STILL)
+
+    def run(self):
+
+        while True:
+            self.game_map.get_frame()
+
+            moves = [self.assign_move(square) for square in self.game_map if square.owner == self.my_id]
+            hlt.send_frame(moves)
+
+
+class RandomBot(Bot):
+    def __init__(self):
+        super(RandomBot, self).__init__("RandomBot")
+
+    def assign_move(self, square):
+        return Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL)))
+
+
+class BlobBot(Bot):
+    def __init__(self):
+        super(BlobBot, self).__init__("BlobBot")
+
+    def assign_move(self, square):
+        for direction, neighbor in enumerate(self.game_map.neighbors(square)):
+            if neighbor.owner != self.my_id and neighbor.strength < square.strength:
+                return Move(square, direction)
+
+        if all(neighbor.owner == self.my_id for neighbor in self.game_map.neighbors(square)):
+            return Move(square, random.choice((NORTH, WEST, SOUTH, EAST)))
+
+        return Move(square, STILL)
