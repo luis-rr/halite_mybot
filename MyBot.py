91585a7b14305d449a7948c1929dc07925f1589e
Luis Riquelme <jluis.riquelme@gmail.com>
2016-12-12 20:58:18 +0100

    blob bot

--- MyBot.py
+++ MyBot.py
@@ -3,10 +3,32 @@ from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
 import random
 
 
-myID, game_map = hlt.get_init()
-hlt.send_init("PythonBot")
+class Hive(object):
+    def __init__(self):
 
-while True:
-    game_map.get_frame()
-    moves = [Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL))) for square in game_map if square.owner == myID]
-    hlt.send_frame(moves)
+        self.my_id, self.game_map = hlt.get_init()
+        hlt.send_init("PythonBot")
+
+    def assign_move(self, square):
+        for direction, neighbor in enumerate(self.game_map.neighbors(square)):
+            if neighbor.owner != self.my_id and neighbor.strength < square.strength:
+                return Move(square, direction)
+
+        if all(neighbor.owner == self.my_id for neighbor in self.game_map.neighbors(square)):
+            return Move(square, random.choice((NORTH, WEST, SOUTH, EAST)))
+
+        return Move(square, STILL)
+
+
+def run():
+
+    hive = Hive()
+
+    while True:
+        hive.game_map.get_frame()
+
+        moves = [hive.assign_move(square) for square in hive.game_map if square.owner == hive.my_id]
+        hlt.send_frame(moves)
+
+
+run()
